ebfc0ff710919c93702a9c7723605eac04bdf354
beautifuldonkey <beautifuldonkey9>
2015-06-24 15:05:21 -0700

    adding getSkillsByName() method to SkillList.java ListView added to profession detail layout to display profession related skills

--- app/src/main/java/beautifuldonkey/survivorsguide/Data/SkillList.java
+++ app/src/main/java/beautifuldonkey/survivorsguide/Data/SkillList.java
@@ -12,6 +12,29 @@ public class SkillList {
 
     public static List<Skill> getSkillList(){ return skillList; }
 
+    public List<Skill> getSkillsByName(String skillName){
+        List<Skill> skills = new ArrayList();
+
+        if(skillName.contains(",")){
+            String [] skillNames = skillName.split(",");
+            for(int i =0; i < skillNames.length; i++){
+                for(int j=0; j<skillList.size();j++){
+                    if(skillNames[i] == skillList.get(j).getName()){
+                        skills.add(skillList.get(j));
+                    }
+                }
+            }
+        }else{
+            for(int j=0; j<skillList.size();j++){
+                if(skillName == skillList.get(j).getName()){
+                    skills.add(skillList.get(j));
+                }
+            }
+        }
+
+        return skills;
+    }
+
     static {
         skillList.add(new Skill("Parry", 5, "Melee Nope"));
         skillList.add(new Skill("Avoid", 5, "Ranged Nope"));
