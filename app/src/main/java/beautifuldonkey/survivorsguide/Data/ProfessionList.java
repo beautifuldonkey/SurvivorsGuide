db2401068ba1df04f583751f7905436ea155bbdb
beautifuldonkey <beautifuldonkey9>
2015-07-26 17:06:26 -0700

    ProfessionList.java -adding build cost for profession skills to static data sets

--- app/src/main/java/beautifuldonkey/survivorsguide/Data/ProfessionList.java
+++ app/src/main/java/beautifuldonkey/survivorsguide/Data/ProfessionList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * provides profession data & accessor methods
  * Created by Jed on 6/19/2015.
  */
 public class ProfessionList {
@@ -13,7 +14,9 @@ public class ProfessionList {
     public static List<Profession> getProfessionList(){ return professionList;}
 
     static{
-        professionList.add(new Profession("Soldier", "Melee badass", "Avoid,Break Shield,Charge,Double-Tap,Florentine,Force Barricade,Interfere,Melee Weapon Expert,Melee Weapon Large,Melee Weapon Standard,Nail,Sever,Shield,Throwing-Javelins", "N/A"));
-        professionList.add(new Profession("Gaurd", "Melee badass", "Barricade,Blind Fighting,Bounce,Break Shield,Break Weapon,Carry,Fearful Glare,Force Barricade,Interfere,Knock Down,Mangle Limb,Melee Weapon Standard,Parry,Shield,Wide Strike", "N/A"));
+        professionList.add(new Profession("Soldier", "Melee badass", "Avoid,Break Shield,Charge,Double-Tap,Florentine,Force Barricade,Interfere,Melee Weapon Expert,Melee Weapon Large,Melee Weapon Standard,Nail,Sever,Shield,Throwing-Javelins", "N/A"
+        ,"3,6,6,6,6,6,3,6,3,3,3,3,3,3"));
+        professionList.add(new Profession("Gaurd", "Melee badass", "Barricade,Blind Fighting,Bounce,Break Shield,Break Weapon,Carry,Fearful Glare,Force Barricade,Interfere,Knock Down,Mangle Limb,Melee Weapon Standard,Parry,Shield,Wide Strike", "N/A"
+        ,"3,6,6,6,6,3,3,3,3,6,6,3,3,3,6"));
     }
 }
