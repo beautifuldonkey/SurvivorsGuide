109fe7eb49b1a11b461d959b998dda63df43f861
beautifuldonkey <beautifuldonkey949@gmail.com>
2016-02-17 21:43:17 -0800

    CharacterNewActivity.java -removing unnecessary setText call
    activity_character_new.xml
    -adding comments
    -updating text string reference for character name
    
    added layout resources to provide dynamic ux
    -xlarge/activity_character_new.xml
    -large/activity_character_new.xml
    -small/activity_character_new.xml
    
    strings.xml
    -added new string value for new character names

--- app/src/main/java/beautifuldonkey/survivorsguide/CharacterNewActivity.java
+++ app/src/main/java/beautifuldonkey/survivorsguide/CharacterNewActivity.java
@@ -56,7 +56,6 @@ public class CharacterNewActivity extends AppCompatActivity {
         final Context context = getApplicationContext();
 
         final EditText charName = (EditText) findViewById(R.id.characterName);
-        charName.setText("Enter character name");
         charName.setTextColor(Color.BLACK);
 
         final TextView charBuild = (TextView) findViewById(R.id.newCharacterBuild);
