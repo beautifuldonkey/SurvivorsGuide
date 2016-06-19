4e2ee866d6c7e9ad902df1d83c75977191a1d6f7
beautifuldonkey <beautifuldonkey949@gmail.com>
2016-06-19 04:08:53 -0700

    OptionsActivity.java -displaying text size dimension in activity layout
    activity_options.xml
    -centering text size in parent

--- app/src/main/java/beautifuldonkey/survivorsguide/OptionsActivity.java
+++ app/src/main/java/beautifuldonkey/survivorsguide/OptionsActivity.java
@@ -2,6 +2,7 @@ package beautifuldonkey.survivorsguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class OptionsActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_options);
+
+    TextView txtTextSize = (TextView) findViewById(R.id.textSize);
+    int dp = (int) (getResources().getDimension(R.dimen.charActivityText) / getResources().getDisplayMetrics().density);
+    txtTextSize.setText(String.valueOf(dp));
   }
 }
