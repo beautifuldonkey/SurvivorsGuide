ea8adbbdb27010324ee31a126ace8168785b00f6
beautifuldonkey <beautifuldonkey949@gmail.com>
2017-01-02 14:14:18 -0800

    Strain.java -adding description property to class
    StrainList.java
    -adding description string to static strain data
    
    StrainDetailActivity.java
    -displaying strain description on screen
    
    activity_strain_detail.xml
    -updating layout to include strain description

--- app/src/main/java/beautifuldonkey/survivorsguide/Data/StrainList.java
+++ app/src/main/java/beautifuldonkey/survivorsguide/Data/StrainList.java
@@ -28,26 +28,28 @@ public class StrainList {
 
   static {
 
-    strainList.add(new Strain("Bay Walker", 8, 10, 4, "Analyze Creature,Double Tap,First Aide,Instruct,Literacy,Parry"));
-    strainList.add(new Strain("Diesel Jock", 10, 10, 3, "Balance,Bolt Action,Forging the Future,Patch Job,Trade Ties,Melee Weapon Standard"));
-    strainList.add(new Strain("Full Dead", 20, 10, 1, "Big Dig,Check Quality,Income,Lie,Literacy,Torture"));
-    strainList.add(new Strain("Genjian", 6, 6, 5, "Analyze Creature,Bow,Brawling,Lore: Mon Histories,Melee Weapon Standard,Sailing,Throwing"));
-    strainList.add(new Strain("Iron Slave", 7, 4, 4, "Brawling,Carry,Escape Bonds,Iron Fists,Refuse,Rescue,Scrounge"));
-    strainList.add(new Strain("Lacarian", 10, 5, 5, "Alert,Blind Fighting,Chase,Chop,Force Barricade,Melee Weapon Small,Scrounge,Take Down"));
-    strainList.add(new Strain("Merican", 13, 10, 2, "Bolt-Action,Brawling,Melee Weapon Large,Melee Weapon Two-Handed,Throwing,Throwing Javelins"));
-    strainList.add(new Strain("Nation Of Accensor", 8, 8, 5, "Building Tomorrow,Challenge,Faith Healing,First Aide,Mind Resistance,Patch Job"));
-    strainList.add(new Strain("Natural One", 10, 6, 4, "Cure Toxins,Melee Weapon Large,Melee Weapon Small,Melee Weapon Standard,Melee Weapon Two-Handed,Throwing,Throwing Javelins"));
-    strainList.add(new Strain("Pure Blood", 6, 12, 3, "Backstab,Bolt-Action,Charisma,Cheat,Check Value,Income,Literacy"));
-    strainList.add(new Strain("Reclaimer", 8, 13, 2, "Balance,Carry,Charge,Melee Weapon Standard,Hunters Mark,Avoid"));
-    strainList.add(new Strain("The Red Star", 6, 6, 6, "Avoid,Barricade,Bomb Awareness,Brewing,Fearful Glare,Frightening Force,Melee Weapon Large"));
-    strainList.add(new Strain("Remnant", 5, 5, 6, "No Strain Skills"));
-    strainList.add(new Strain("Retrograde", 10, 10, 4, "Barricade,Cover of Night,Disguise,Escape Bonds,Feign Death,Melee Weapon Standard,Scrounge"));
-    strainList.add(new Strain("Rover", 8, 8, 4, "Bartender's Tongue,Check Your Sleeves,Head Shrink,Melee Weapon Small,Refuse,Scrounge"));
-    strainList.add(new Strain("Salt Wise", 13, 8, 3, "Sailing,Fishing,Lore: Aquatic,Melee Weapon Small,Guild Membership,Lie,Escape Bonds"));
-    strainList.add(new Strain("Semper Mort", 10, 10, 3, "Brawling,Charisma,Chase,Iron Fists,Nerve Punch,Tie Binds"));
-    strainList.add(new Strain("Solestros", 6, 6, 4, "Income,Deep Pockets,Literacy,Charisma,Balance,Melee Weapon Standard,Refuse"));
-    strainList.add(new Strain("Unborn of Teixiptla", 4, 4, 7, "Melee Weapon Two-Handed,Psionic Basic Skill,Throwing Javelins"));
-    strainList.add(new Strain("Vegasian", 5, 10, 5, "Backstab,Black Market Connections,Cheat,Entertain,Lie,Literacy"));
-    strainList.add(new Strain("Yorker", 15, 5, 4, "Barricade,Bomb Awareness,Chase,Entertain,Interfere,Melee Weapon Standard"));
+    strainList.add(new Strain("Bay Walker", 8, 10, 4, "Analyze Creature,Double-Tap,First Aide,Instruct,Literacy,Parry",
+        "Bay Walkers are fast learners and quickly pick up new concepts. They know exactly how to push the buttons of all Yorkers. Since they avoided most of the radiation in past generations they suffer double damage from all Toxic/Poison sources. They can never possess the following Professions: Sawbones, Primitive, or Thug."));
+    strainList.add(new Strain("Diesel Jock", 10, 10, 3, "Balance,Bolt Action,Forging the Future,Patch Job,Trade Ties,Melee Weapon Standard",
+        "With the circles Diesel Jocks travel in they can utilize Trade Ties to swap Herb for Scrap at the same level restrictions. Due to this constantly moving around the Income skill generates currency at two levels less than what they posses. They can never possess the following Professions: Farmer, Politician, or Publican."));
+    strainList.add(new Strain("Full Dead", 20, 10, 1, "Big Dig,Check Quality,Income,Lie,Literacy,Torture",""));
+    strainList.add(new Strain("Genjian", 6, 6, 5, "Analyze Creature,Bow,Brawling,Lore: Mon Histories,Melee Weapon Standard,Sailing,Throwing",""));
+    strainList.add(new Strain("Iron Slave", 7, 4, 4, "Brawling,Carry,Escape Bonds,Iron Fists,Refuse,Rescue,Scrounge",""));
+    strainList.add(new Strain("Lacarian", 10, 5, 5, "Alert,Blind Fighting,Chase,Chop,Force Barricade,Melee Weapon Small,Scrounge,Take Down",""));
+    strainList.add(new Strain("Merican", 13, 10, 2, "Bolt-Action,Brawling,Melee Weapon Large,Melee Weapon Two-Handed,Throwing,Throwing Javelins",""));
+    strainList.add(new Strain("Nation Of Accensor", 8, 8, 5, "Building Tomorrow,Challenge,Faith Healing,First Aide,Mind Resistance,Patch Job",""));
+    strainList.add(new Strain("Natural One", 10, 6, 4, "Cure Toxins,Melee Weapon Large,Melee Weapon Small,Melee Weapon Standard,Melee Weapon Two-Handed,Throwing,Throwing Javelins",""));
+    strainList.add(new Strain("Pure Blood", 6, 12, 3, "Backstab,Bolt-Action,Charisma,Cheat,Check Value,Income,Literacy",""));
+    strainList.add(new Strain("Reclaimer", 8, 13, 2, "Balance,Carry,Charge,Melee Weapon Standard,Hunters Mark,Avoid",""));
+    strainList.add(new Strain("The Red Star", 6, 6, 6, "Avoid,Barricade,Bomb Awareness,Brewing,Fearful Glare,Frightening Force,Melee Weapon Large",""));
+    strainList.add(new Strain("Remnant", 5, 5, 6, "No Strain Skills",""));
+    strainList.add(new Strain("Retrograde", 10, 10, 4, "Barricade,Cover of Night,Disguise,Escape Bonds,Feign Death,Melee Weapon Standard,Scrounge",""));
+    strainList.add(new Strain("Rover", 8, 8, 4, "Bartender's Tongue,Check Your Sleeves,Head Shrink,Melee Weapon Small,Refuse,Scrounge",""));
+    strainList.add(new Strain("Salt Wise", 13, 8, 3, "Sailing,Fishing,Lore: Aquatic,Melee Weapon Small,Guild Membership,Lie,Escape Bonds",""));
+    strainList.add(new Strain("Semper Mort", 10, 10, 3, "Brawling,Charisma,Chase,Iron Fists,Nerve Punch,Tie Binds",""));
+    strainList.add(new Strain("Solestros", 6, 6, 4, "Income,Deep Pockets,Literacy,Charisma,Balance,Melee Weapon Standard,Refuse",""));
+    strainList.add(new Strain("Unborn of Teixiptla", 4, 4, 7, "Melee Weapon Two-Handed,Psionic Basic Skill,Throwing Javelins",""));
+    strainList.add(new Strain("Vegasian", 5, 10, 5, "Backstab,Black Market Connections,Cheat,Entertain,Lie,Literacy",""));
+    strainList.add(new Strain("Yorker", 15, 5, 4, "Barricade,Bomb Awareness,Chase,Entertain,Interfere,Melee Weapon Standard",""));
   }
 }
